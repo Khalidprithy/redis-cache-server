7fb028067ca98a6dad1755aceb8499c9aabe3ef8
Khalidprithy <khalidprithy@gmail.com>
2024-02-25 23:00:58 +0600

    cache updated

--- index.js
+++ index.js
@@ -77,6 +77,39 @@ app.get('/data-10', cacheMiddleware, async (req, res) => {
     }
 });
 
+
+app.get('/recipe/:fooditem', (req, res) => {
+    try {
+        const foodItem = req.params.fooditem;
+
+        // Check the redis store for the data first
+        client.get(foodItem, async (err, recipe) => {
+            if (recipe) {
+                return res.status(200).send({
+                    error: false,
+                    message: `Recipe for ${foodItem} from the cache`,
+                    data: JSON.parse(recipe)
+                })
+            } else { // When the data is not found in the cache then we can make request to the server
+
+                const recipe = await axios.get(`http://www.recipepuppy.com/api/?q=${foodItem}`);
+
+                // save the record in the cache for subsequent request
+                client.setex(foodItem, 1440, JSON.stringify(recipe.data.results));
+
+                // return the result to the client
+                return res.status(200).send({
+                    error: false,
+                    message: `Recipe for ${foodItem} from the server`,
+                    data: recipe.data.results
+                });
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
 // Start the server
 app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
